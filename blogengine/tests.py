1309464005476eddb0e0138312dfe7969ae274f1
reg3x <diegoaguilera85@gmail.com>
2014-07-19 10:23:10 -0430

    added test for admin auth

--- blogengine/tests.py
+++ blogengine/tests.py
@@ -1,8 +1,9 @@
-from django.test import TestCase
+from django.test import TestCase, LiveServerTestCase, Client
 from django.utils import timezone
 from blogengine.models import Post
 
 
+# Create your tests here.
 class PostTest(TestCase):
     def test_create_post(self):
         # Create the post
@@ -31,3 +32,56 @@ class PostTest(TestCase):
         self.assertEquals(only_post.pub_date.hour, post.pub_date.hour)
         self.assertEquals(only_post.pub_date.minute, post.pub_date.minute)
         self.assertEquals(only_post.pub_date.second, post.pub_date.second)
+
+
+class AdminTest(LiveServerTestCase):
+    fixtures = ['users.json']
+
+
+    def test_login(self):
+        # Create client
+        c = Client()
+
+        # Get login page
+        response = c.get('/admin/')
+
+        # Check response code
+        self.assertEquals(response.status_code, 200)
+
+        # Check 'Log in' in response
+        self.assertTrue('Log in' in response.content)
+
+        # Log the user in
+        c.login(username='andersonthomas385', password="da4373az")
+
+        # Check response code
+        response = c.get('/admin/')
+        self.assertEquals(response.status_code, 200)
+
+        # Check 'Log out' in response
+        self.assertTrue('Log out' in response.content)
+
+
+    def test_logout(self):
+        # Create client
+        c = Client()
+
+        # Log in
+        c.login(username='andersonthomas385', password="da4373az")
+
+        # Check response code
+        response = c.get('/admin/')
+        self.assertEquals(response.status_code, 200)
+
+        # Check 'Log out' in response
+        self.assertTrue('Log out' in response.content)
+
+        # Log out
+        c.logout()
+
+        # Check response code
+        response = c.get('/admin/')
+        self.assertEquals(response.status_code, 200)
+
+        # Check 'Log in' in response
+        self.assertTrue('Log in' in response.content)

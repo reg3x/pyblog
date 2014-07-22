01c8451fc9c7e59ea24879cae88516f59373d9a2
reg3x <diegoaguilera85@gmail.com>
2014-07-22 15:58:06 -0430

    Amended settings to work on desktop and Heroku

--- pyblog/settings.py
+++ pyblog/settings.py
@@ -73,8 +73,8 @@ WSGI_APPLICATION = 'pyblog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.',
+        'NAME': '',
     }
 }
 
@@ -100,3 +100,21 @@ STATIC_URL = '/static/'
 #Template directory
 
 TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
+
+# Heroku config
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config(default="sqlite:///db.sqlite3")
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+STATIC_ROOT = 'staticfiles'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
